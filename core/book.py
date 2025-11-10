0f415a91800f9d27eeea0361c579bc2165d18908
JosephRer <jrechdiner2002@gmail.com>
2025-11-10 11:00:46 +0200

    finished book class

---
+++ core/book.py
@@ -0,0 +1,22 @@
+class Book:
+    ISBN_counter = "9780321342"
+
+    def __init__(self, title: str, author: str):
+        self.title = title
+        self.author = author
+        self.ISBN = Book.ISBN_counter
+        self.is_available = True
+
+        Book.ISBN_counter = int(Book.ISBN_counter) + 1
+
+    def __str__(self):
+        return f"Title: {self.title} | Author: {self.author} | ISBN: {self.ISBN} | {self.checking_available()}"
+
+    def get_isbn(self):
+        return self.ISBN
+    
+    def checking_available(self):
+        if self.is_available:
+            return f"Available"
+        return f"Not Available"
+
